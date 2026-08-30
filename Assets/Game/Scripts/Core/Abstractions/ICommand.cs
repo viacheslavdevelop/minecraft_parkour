9e4fcc5ad6342315429e77c042359cf116ccacc8
Viacheslav Dev <viledalton@gmail.com>
2026-08-30 22:24:49 +0600

    feat: core

---
+++ Assets/Game/Scripts/Core/Abstractions/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.Scripts.Core.Abstractions
+{
+    public interface ICommand
+    {
+        public void Execute();
+    }
+}
---
+++ Assets/Game/Scripts/Core/GameLoop.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Game.Scripts.Core
+{
+    public class GameLoop : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
